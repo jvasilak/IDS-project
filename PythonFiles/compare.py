27fd0b795d3fc2a2f2ca6d400041558d7ced778b
jvasilak <71355888+jvasilak@users.noreply.github.com>
2023-04-24 21:08:41 -0400

    Delete compare.py

--- PythonFiles/compare.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from numpy.linalg import norm
-# a and b are both numpy arrays, this function should be used to compute one factor, not the combination
-cosine_similarity(a, b):
-    return (np.dot(a, b)/())
